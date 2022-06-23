3709ad7cc3e8d52f0216f116c74fe2cf86407c95
korobun <korobun@gmail.com>
2022-06-23 16:41:34 +0300

    HW_7 : DateUtil

--- src/com/alxkor/webapp/model/Link.java
+++ src/com/alxkor/webapp/model/Link.java
@@ -12,11 +12,37 @@ public class Link {
         this.url = url;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
     @Override
     public String toString() {
         return "Link{" +
                 "title='" + title + '\'' +
                 ", url='" + url + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Link link = (Link) o;
+
+        if (!title.equals(link.title)) return false;
+        return url != null ? url.equals(link.url) : link.url == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
     }
 }
